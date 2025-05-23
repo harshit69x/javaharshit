cdfe5f008168b2462826c8ec7d788df79bcbabc3
harshit69x <mohantyharshit303@gmail.com>
2025-05-23 19:29:07 +0530

    dss

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+
     }
 }
 
